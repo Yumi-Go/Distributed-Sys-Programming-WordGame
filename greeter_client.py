88a5073c57597a6b5a36c57d25121f523d841ab2
Yumi Go <yumigoyumi@gmail.com>
2022-10-29 18:32:07 +0100

    almost finish

--- greeter_client.py
+++ greeter_client.py
@@ -21,6 +21,15 @@ def run():
             init_phrase = init_phrase.init_result_phrase.strip().upper()
             print("init_phrase Result before loop: " + init_phrase)  # for check
 
+            temp_list = list(chosen_phrase)
+            list_for_count = []
+            for l in temp_list:
+                if l not in temp_list:
+                    list_for_count.append(l)
+            print(list_for_count) # for check
+
+            num_character = len(chosen_phrase)
+            counter = 0
             while True:
                 guess = input("Enter a letter: ").upper()
                 changed_phrase = stub.ChangeLetter(wordgame_pb2.LetterRequest(chosen_phrase=chosen_phrase,
@@ -31,9 +40,29 @@ def run():
                 print("Result: " + init_phrase) # for check
                 print("Answer: " + chosen_phrase) # for check
 
+                counter += 1
+
                 if init_phrase == chosen_phrase:
                     print()
                     print("*********** Success! ***********")
+
+                    if counter == len(list_for_count):
+                        print("Score: 100%")
+                        print("Great! You succeeded in only one try every letter!")
+                    elif counter >= (2 * len(list_for_count)):
+                        score = 100 / counter
+                        print("Score: " + score)
+                        if score >= 70:
+                            print("Score: " + score)
+                            print("Well Done!")
+                        elif score > 50:
+                            print("Score: " + score)
+                            print("Not good")
+                        else:
+                            print("Score: " + score)
+                            print("Too Bad~ Practice more!")
+
+
                     break
             print()
             choice = input("Go again? (Y/N) ")
